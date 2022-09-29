9fe9ce9ebc12b4a446a979f8ff7efa8370e86698
kodnennsy <lkvlkvlkv2003@yandex.ru>
2022-09-29 18:41:19 +0300

    punkt 3 modified

--- src/ru/mirea/lab2/punkt3/Circle.java
+++ src/ru/mirea/lab2/punkt3/Circle.java
@@ -12,13 +12,7 @@ public class Circle extends Point {
         this.r = r;
     }
 
-    public double getX(){return x;}
 
-    public void setX(double x){ this.x = x;}
-
-    public double getY(){return y;}
-
-    public void setY(double y){ this.y = y;}
 
 
 }
--- src/ru/mirea/lab2/punkt3/Point.java
+++ src/ru/mirea/lab2/punkt3/Point.java
@@ -7,7 +7,7 @@ public class Point {
     public Point(){
         x = 0;
         y = 0;
-    };
+    }
 
     public Point(double x, double y){
         this.x = x;
@@ -20,8 +20,4 @@ public class Point {
     public double getY(){return y;}
 
     public void setY(double y){ this.y = y;}
-
-
-
-
 }
